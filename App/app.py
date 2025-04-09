e0ce2ff999d2284050a11359a17e0148abd2a69f
JisnaP <jisna12@gmail.com>
2025-04-09 16:22:44 +0300

    CI/CD added

--- App/app.py
+++ App/app.py
@@ -1,10 +1,12 @@
-import scikit_learn
+import sklearn
 import skops.io as sio
+import os
+os.environ.pop("SSL_CERT_FILE", None)
 import gradio as gr
 
 
 import warnings
-from sklearn.execeptions import InconsistentVersionWarning
+from sklearn.exceptions import InconsistentVersionWarning
 trusted_types=[
     "sklearn.pipeline.Pipeline",
     "sklearn.preprocessing.StandardScaler",
@@ -15,7 +17,7 @@ trusted_types=[
     "sklearn.ensemble.RandomForestClassifier",
     "numpy.dtype"
 ]
-pipeline=sio.load("./model/drug_pipeline.siops",trusted_types=trusted_types)
+pipeline=sio.load("./model/drug_pipeline.skops",trusted=trusted_types)
 
 def predict_drug(age,sex,blood_pressure,cholestrol,na_to_k_ratio):
     """
@@ -33,17 +35,18 @@ def predict_drug(age,sex,blood_pressure,cholestrol,na_to_k_ratio):
     """
     features=[age,sex,blood_pressure,cholestrol,na_to_k_ratio]
     predicted_drug=pipeline.predict([features])[0]
-    label= print(f"Predicted_drug: {predicted_drug}")
-    return label
+    print(f"Predicted_drug: {predicted_drug}")
+    return predicted_drug
 
-inputs=[
-    gr.Slider(15,74,step=1,label="Age"),
-    gr.Radio(["M","F"],label="Sex"),
-    gr.Radio(["High","Low","Normal"],label="Blood_pressure"),
-    gr.Radio(["High","Normal"],label="Cholestrol"),
-    gr.Slider(6.2,38.2,step=0.1,label="Na_to_K")
+inputs = [
+    gr.Slider(15, 74, step=1, label="Age"),
+    gr.Radio(["M", "F"], label="Sex"),
+    gr.Radio(["HIGH", "LOW", "NORMAL"], label="Blood Pressure"),
+    gr.Radio(["HIGH", "NORMAL"], label="Cholesterol"),
+    gr.Slider(6.2, 38.2, step=0.1, label="Na_to_K"),
 ]
-outputs=[gr.label(num_top_classes=5)]
+
+outputs=[gr.Label(num_top_classes=5)]
 examples = [
     [30, "M", "HIGH", "NORMAL", 15.4],
     [35, "F", "LOW", "NORMAL", 8],
